2950caf853bd1060c3e3b405e05fe2d52bc36812
Sreedevi Krishnan <sreedevik29@gmail.com>
2017-08-01 23:03:32 -0400

    Surround in main() function

--- to-do-script.py
+++ to-do-script.py
@@ -1,15 +1,17 @@
-data = {}	
-active = True
+def main():
+    data = {}
+    active = True
+    while active:
+        activity = raw_input("Add an activity: ")
+        category = raw_input("What type of activity is this? ")
+        if data.keys().__contains__(category.lower()):
+            data[category.lower()].append(activity)
+        else:
+            data[category.lower()] = [activity]
+        more_activities = raw_input("Do you have more? Type 'yes' or 'y' to add more activities the to do list or type 'no' or 'n' to stop making the list: ")
+        if more_activities == "no" or more_activities == "n":
+            print("Here are the list of things you have to do: " + str(data))
+            active = False
 
-while active:
-    activity = raw_input("Add an activity: ")
-    category = raw_input("What type of activity is this? ")
-    if data.keys().__contains__(category.lower()):
-        data[category.lower()].append(activity)
-    else:
-        data[category.lower()] = [activity]
-    more_activities = raw_input("Do you have more? Type 'yes' or 'y' to add more activities the to do list or type 'no' or 'n' to stop making the list: ")
-    if more_activities == "no" or more_activities == "n":
-        print("Here are the list of things you have to do: " + str(data))
-        active = False
-
+if __name__ == '__main__':
+    main()
