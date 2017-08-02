708f80c2b47614057f908c25da7b2afe4fea8e28
Sreedevi Krishnan <sreedevik29@gmail.com>
2017-08-01 22:45:02 -0400

    Change data storage to lists

--- to-do-script.py
+++ to-do-script.py
@@ -1,19 +1,23 @@
-to_do = {"Work": "", 
-		"School": "",
-		"Exercise": ""}			
+to_do = {"Work": [], 
+		"School": [],
+		"Exercise": []}			
 active = True
 
 while active:
     user_input = raw_input("Add an activity: ")
     input_type = raw_input("What type of activity is this? ")
     if input_type.lower() in ("work", "job"):
-    	to_do.update({"Work": user_input})
+    	to_do['Work'].append(user_input)
+        print(to_do)
     elif input_type.lower() in ("exercise", "workout"):
-    	to_do.update({"Exercise": user_input})
+        to_do['Exercise'].append(user_input)
+        print(to_do)
     elif input_type.lower() in ("school", "uni"):
-    	to_do.update({"School": user_input})
+        to_do['School'].append(user_input)
+        print(to_do)
     else:
-    	to_do.update({input_type: user_input})
+        to_do[input_type] = [user_input]
+        print(to_do)
     more_activities = raw_input("Do you have more? Type 'yes' or 'y' to add more activities the to do list or type 'no' or 'n' to stop making the list: ")
     if more_activities == "no" or more_activities == "n":
         print("Here are the list of things you have to do: " + str(to_do))
