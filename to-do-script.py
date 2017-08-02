ae2110c4f183af384e5f8ee5f7ab4dc94ad6d40b
Sreedevi Krishnan <sreedevik29@gmail.com>
2017-08-01 23:01:06 -0400

    Simplify category logic

--- to-do-script.py
+++ to-do-script.py
@@ -1,25 +1,15 @@
-to_do = {"Work": [], 
-		"School": [],
-		"Exercise": []}			
+data = {}	
 active = True
 
 while active:
-    user_input = raw_input("Add an activity: ")
-    input_type = raw_input("What type of activity is this? ")
-    if input_type.lower() in ("work", "job"):
-    	to_do['Work'].append(user_input)
-        print(to_do)
-    elif input_type.lower() in ("exercise", "workout"):
-        to_do['Exercise'].append(user_input)
-        print(to_do)
-    elif input_type.lower() in ("school", "uni"):
-        to_do['School'].append(user_input)
-        print(to_do)
+    activity = raw_input("Add an activity: ")
+    category = raw_input("What type of activity is this? ")
+    if data.keys().__contains__(category.lower()):
+        data[category.lower()].append(activity)
     else:
-        to_do[input_type] = [user_input]
-        print(to_do)
+        data[category.lower()] = [activity]
     more_activities = raw_input("Do you have more? Type 'yes' or 'y' to add more activities the to do list or type 'no' or 'n' to stop making the list: ")
     if more_activities == "no" or more_activities == "n":
-        print("Here are the list of things you have to do: " + str(to_do))
+        print("Here are the list of things you have to do: " + str(data))
         active = False
 
