3673c795d6997bd7981ab53fd19b7332680a953b
Sreedevi Krishnan <sreedevik29@gmail.com>
2017-08-07 01:52:41 -0400

    Recode to simplify user experience

--- to-do-script.py
+++ to-do-script.py
@@ -1,45 +1,17 @@
-import pprint
+data = {}   
+active = True
 
-def categoryExists(user_input, data):
-    # Check to see if user_input is a real category name
-    if user_input.lower() in data.keys():
-        return True
-    return False
-
-def getCategoryByAlias(user_input, data):
-    # If not, check to see if user_input is referring to an aliased category
-    for category in data.keys():
-        # If you can find the user_input inside the aliases, 
-        # return the name of the category, because that is
-        # the category that the alias is referring to
-        if user_input.lower() in data[category]['aliases']:
-            return category
-    return ""
-
-def main():
-    data = {}
-    active = True
-    pp = pprint.PrettyPrinter(indent=4)
-    while active:
-        activity = raw_input("Add an activity: ")
-        category_name = raw_input("What category of activity is this? ")
-        if categoryExists(category_name, data):
-            data[category_name.lower()]["activities"].append(activity)
-        elif getCategoryByAlias(category_name, data):
-            aliased_category = getCategoryByAlias(category_name, data)
-            data[aliased_category]["activities"].append(activity)
-        else:
-            aliases = raw_input("List any aliases seperated by commas: ")
-            new_category_data = {
-                'aliases': [alias.strip() for alias in aliases.split(',')],
-                'activities': [activity]
-            }
-            data[category_name.lower()] = new_category_data
-        more_activities = raw_input("Do you have more? Type 'yes' or 'y' to add more activities the to do list or type 'no' or 'n' to stop making the list: ")
-        if more_activities == "no" or more_activities == "n":
-            print("Here are the list of things you have to do: ")
-            pp.pprint(data)
-            active = False
-
-if __name__ == '__main__':
-    main()
+while active:
+    activity = raw_input("Add an activity: ")
+    category = raw_input("What type of activity is this? ")
+    if data.keys().__contains__(category.title()):
+        data[category.title()].append(activity.title())
+    else:
+        data[category.title()] = [activity.title()]
+    more_activities = raw_input("Do you have more to add? Type 'yes' or 'y' to add more activities the to do list or type 'no' or 'n' to stop making the list: ")
+    if more_activities == "yes" or more_activities == "y":
+        print("Here are the list of things to do so far with the categories: ")
+        print(data)
+    else: 
+        print("Here are the list of things you have to do: " + str(data))
+        active = False
